8f8e187b70a32d7f820bb9d338394429c409be2f
Vepp <vinipontarolla@gmail.com>
2023-01-25 14:15:47 +0100

    Add PostList initial template

--- src/pages/posts/PostList.js
+++ src/pages/posts/PostList.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import styles from '../../styles/PostList.module.css'
 
 const PostList = () => {
   return (
-    <div>Display Posts</div>
+    <>
+        <Card className={styles.Post}>
+            <Card.Img variant="top" src='' />
+            <Card.Body>
+                <Card.Title>Movie Title</Card.Title>
+                <Card.Text>
+                Movie Description
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+
+        </> 
   )
 }
 
