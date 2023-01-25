6f746c49b1a019bc0a362603b47b78837c767f4e
Vepp <vinipontarolla@gmail.com>
2023-01-25 16:27:11 +0100

    Add react router to App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Container } from 'react-bootstrap';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 import PostList from './pages/posts/PostList';
 
 function App() {
   return (
     <div className="App">
-      <PostList />
+      <Container>
+          <Switch>
+            <Route exact path='/' render={() => <PostList />} />
+          </Switch>
+      </Container>
     </div>
   );
 }
--- src/pages/posts/PostList.js
+++ src/pages/posts/PostList.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
 import styles from '../../styles/PostList.module.css'
 
 const PostList = () => {
   return (
     <>
         <Card className={styles.Post}>
-            <Card.Img variant="top" src='' />
+            <Card.Img variant="top" src='https://i.ebayimg.com/images/g/~gUAAOSwIOthElD4/s-l1600.jpg' />
             <Card.Body>
-                <Card.Title>Movie Title</Card.Title>
-                <Card.Text>
-                Movie Description
+                <Card.Title>Harry Potter</Card.Title>
+                <Card.Text className='mt-2'>
+                Genrer: Fiction
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <p className={styles.P}>Created by Vinicius. 23/01/2023</p>
             </Card.Body>
         </Card>
 
