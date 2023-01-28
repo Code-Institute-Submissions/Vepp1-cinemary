6aae61653d1778431224ae659ae62413c80628e1
Vepp <vinipontarolla@gmail.com>
2023-01-28 15:43:41 +0100

    Add navbar base template

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Container } from 'react-bootstrap';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
 import PostList from './pages/posts/PostList';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Container>
           <Switch>
             <Route exact path='/' render={() => <PostList />} />
