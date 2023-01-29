48d7adbe41b1583007e6f4781011a33ac18d30d4
Vepp <vinipontarolla@gmail.com>
2023-01-29 14:40:43 +0100

    Add currentuser context to navbar to display different options

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,18 @@
+import { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext)
+
+  const loggedInNav = (<><Nav.Link href="/">Create Post</Nav.Link>
+                      <Nav.Link href="/">Logout</Nav.Link> </>)
+  const loggedOutNav = (<><Nav.Link href="/signin">Sign in</Nav.Link>
+                      <Nav.Link href="/signup">Sign up</Nav.Link></>)
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -11,19 +20,7 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/signin">Sign in</Nav.Link>
-            <Nav.Link href="/signup">Sign up</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            { currentUser ? loggedInNav : loggedOutNav}
           </Nav>
         </Navbar.Collapse>
       </Container>
