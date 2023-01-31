74b3e4c045df69c0c09d968022b5954cfbca193d
Vepp <vinipontarolla@gmail.com>
2023-01-31 17:30:27 +0100

    Connect postlist page to api and display posts that are on database

--- src/pages/posts/PostList.js
+++ src/pages/posts/PostList.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from 'react-bootstrap/Card'
+import { useLocation } from 'react-router-dom'
+import { axiosReq } from '../../api/axiosDefaults'
 import styles from '../../styles/PostList.module.css'
 
 const PostList = () => {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get('/posts/');
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPosts();
+  }, [pathname]);
+
   return (
     <>
-        <Card className={styles.Post}>
-            <Card.Img variant="top" src='https://i.ebayimg.com/images/g/~gUAAOSwIOthElD4/s-l1600.jpg' />
-            <Card.Body>
-                <Card.Title>Harry Potter</Card.Title>
-                <Card.Text className='mt-2'>
-                Genrer: Fiction
-                </Card.Text>
-                <p className={styles.P}>Created by Vinicius. 23/01/2023</p>
-            </Card.Body>
-        </Card>
-
+    {posts.results?.map((post) => (
+      <Card className={styles.Post}>
+        <Card.Img variant="top" src={post.image} />
+        <Card.Body>
+          <Card.Title>{post.title}</Card.Title>
+          <Card.Text className='mt-2'>
+              Genrer: {post.genrer}
+          </Card.Text>
+          <p className={styles.P}>Created by {post.owner}. 23/01/2023</p>
+        </Card.Body>
+      </Card>))}
         </> 
   )
 }
