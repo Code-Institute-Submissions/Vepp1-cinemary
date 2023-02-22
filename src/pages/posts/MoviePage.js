459c5f454c98330e68f9902f40ec4dcad7dc9cdd
Vepp <vinipontarolla@gmail.com>
2023-02-22 18:55:28 +0100

    Add spinner to moviepage to be displayed while not fully loaded

--- src/pages/posts/MoviePage.js
+++ src/pages/posts/MoviePage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Col, Container, Image, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+import Asset from "../../components/Asset";
 import { useCurrentUser } from "../../context/CurrentUserContext";
 import styles from "../../styles/MoviePage.module.css";
 import Comments from "../comments/Comments";
@@ -11,6 +12,7 @@ const MoviePage = () => {
   const { id } = useParams();
   const [postData, setPostData] = useState({ results: [] });
   const [comments, setComments] = useState({ results: [] });
+  const [hasLoad, setHasLoad] = useState(false);
   const currentUser = useCurrentUser();
 
   useEffect(() => {
@@ -32,56 +34,68 @@ const MoviePage = () => {
           updated_at,
         });
         setComments(comments);
+        setHasLoad(true);
       } catch (error) {
         console.log(error);
       }
     };
+    setHasLoad(false);
     handleMount();
   }, [id]);
 
   return (
-    <Container className="my-5">
-      <Row className="text-justify">
-        <Col>
-          <Image src={postData.image} className={styles.Image} />
-        </Col>
-        <Col>
-          <h1 className="my-3">{postData.title}</h1>
-          <p className="text-left">
-            <strong>Genrer:</strong> {postData.genrer}
-          </p>
-          <p>{postData.content}</p>
-          <p>
-            Created by: <strong>{postData.owner}</strong> on{" "}
-            {postData.created_at}
-          </p>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          {currentUser ? (
-            <CommentsCreate
-              post={id}
-              setPostData={setPostData}
-              setComments={setComments}
-            />
-          ) : null}
+    <>
+      {hasLoad ? (
+        <Container>
+          <Row className="text-justify">
+            <Col>
+              <Image src={postData.image} className={styles.Image} />
+            </Col>
+            <Col>
+              <h1 className="my-3">{postData.title}</h1>
+              <p className="text-left">
+                <strong>Genrer:</strong> {postData.genrer}
+              </p>
+              <p>{postData.content}</p>
+              <p>
+                Created by: <strong>{postData.owner}</strong> on{" "}
+                {postData.created_at}
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              {currentUser ? (
+                <CommentsCreate
+                  post={id}
+                  setPostData={setPostData}
+                  setComments={setComments}
+                />
+              ) : null}
 
-          {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comments
-                key={comment.id}
-                {...comment}
-                setPostData={setPostData}
-                setComments={setComments}
-              />
-            ))
-          ) : (
-            <p>none</p>
-          )}
-        </Col>
-      </Row>
-    </Container>
+              {comments.results.length ? (
+                comments.results.map((comment) => (
+                  <Comments
+                    key={comment.id}
+                    {...comment}
+                    setPostData={setPostData}
+                    setComments={setComments}
+                  />
+                ))
+              ) : (
+                <Container>
+                  <Asset spinner />
+                </Container>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      ) : (
+        <Container className="mt-5">
+          <Asset spinner />
+        </Container>
+      )}
+    </>
   );
 };
 
